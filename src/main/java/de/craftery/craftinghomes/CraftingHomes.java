622bc8530a3db4d164947525776411f21afa54b6
Crafter_Y <mrcrafter.yt1@gmail.com>
2023-07-06 19:08:28 +0200

    Init

---
+++ src/main/java/de/craftery/craftinghomes/CraftingHomes.java
@@ -0,0 +1,18 @@
+package de.craftery.craftinghomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftingHomes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        this.getLogger().fine("CraftingHomes is starting upe!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
